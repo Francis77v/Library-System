68347b43cc9439984665689addc93147773a5cff
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2025-09-18 22:07:58 +0800

    Initial Commit

---
+++ LibrarySystem/Program.cs
@@ -0,0 +1,16 @@
+﻿bool condition = true;
+
+while (condition)
+{
+    Console.WriteLine($@"
+    Choose an Option:
+    1. Add Book
+    2. Add Magazine
+    3. Show All Items
+    4. Borrow Item
+    5. Return Item
+    6. Exit
+");
+
+    int Option = Convert.ToInt32(Console.ReadLine());
+}
